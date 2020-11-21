41db2f4ff05f8c936b9c3066d430b3dde05141bb
Shen <73876759+Shenixreal@users.noreply.github.com>
2020-11-21 13:50:58 +0530

    Add files via upload
    I created this simple bot to practice with Python. Earlier I added queries and responses in both lowercase and uppercase letters to the lists, so it can respond even if you use uppercase letters for input. But now I've made the inputs to be taken in lowercase.

---
+++ GhostBot.py
@@ -0,0 +1,258 @@
+#created by Shenixreal
+
+import random
+
+
+#Greetings
+greetings = ['hola','hello', 'hi','hey','hola!','hello!','hi!','hey!','yo','yo!']
+random_greeting = random.choice(greetings)
+
+greetings2=['bye!','bye','good bye!','good bye','salut']
+random_greeting2=random.choice(greetings2)
+greetings02=['Bye!','Okay bye!','bye bye']
+random_greeting02=random.choice(greetings02)
+
+greetings3=['see you later!','see you later','see you']
+random_greeting3=random.choice(greetings3)
+greetings03=['You too! Take care, bye!']
+random_greeting03=random.choice(greetings03)
+
+greetings4=['Happy birthday!','Many happy returns of the day!']
+random_greeting4=random.choice(greetings4)
+
+greetings5=['happy new year!','happy new year','merry christmas','merry christmas!']
+random_greeting5=random.choice(greetings5)
+greetings05=['Wish you the same! :-)']
+random_greeting05=random.choice(greetings05)
+
+greetings6=['thanks!','thanks','thank you!','thank you','thanks a lot!','thanks a lot']
+random_greeting6=random.choice(greetings6)
+greetings06=['No problem!']
+random_greeting06=random.choice(greetings06)
+
+greetings7=["xoxo"]
+random_greeting7=random.choice(greetings7)
+greetings07=["xoxo *hugs* Love you! *hugs* xoxo"]
+random_greeting07=random.choice(greetings07)
+
+#Random_questions
+question1 = ['how are you?','how are you doing?','how are you','how are you doing',"how's life?","how's life"]
+response1 = ["I'm doing well","I'm fine"]
+random_response1 = random.choice(response1)
+
+question2=["what's up?","what's up"]
+response2=['The sky haha','Nothing much',"Ummm...I don't know","Nothing's up"]
+random_response2 = random.choice(response2)
+
+question3=['What can you do?','what can you do?','what can you do','What can you do']
+response3=["I can chat with you and answer your random questions. But I'm not very intelligent like you. If you want to know some cool codes type 'codes'"]
+random_response3 = random.choice(response3)
+
+question4=["What's your name?","what's your name?","what's your name","What's your name"]
+response4=["My name is GhostBot"]
+random_response4 = random.choice(response4)
+
+question5=['How old are you?','how old are you?','How old are you','how old are you']
+response5=["I'm not sure about my age. I guess I'm young."]
+random_response5 = random.choice(response5)
+
+question6=['Who made you?','who made you?','who created you?','Who created you?','Who made you','who made you','who created you','Who created you']
+response6=['Someone awesome']
+random_response6 = random.choice(response6)
+
+question7=['Who are you?','who are you?','Who are you','who are you']
+response16=["I'm a bot. A bot is a computer program which can talk/chat like humans :)"]
+random_response16 = random.choice(response16)
+
+#
+exclaim=["Today is my birthday","Today is my b'day","It's my birthday","It's my b'day","it's my birthday","it's my b'day"]
+random_exclaim=random.choice(exclaim)
+
+exclaim6=["Cool!",'cool!','cool','Cool',"That's cool!","that's cool!"]
+random_exclaim6=random.choice(exclaim6)
+
+exclaim7=["Sorry!","sorry!","sorry","Sorry","I'm sorry!","i'm sorry!","So sorry","so sorry"]
+random_exclaim7=random.choice(exclaim7)
+
+response17=["It's okay","It's okay, no worries"]
+random_response17=random.choice(response17)
+
+exclaim8=["Raindrop","raindrop"]
+random_exclaim8=random.choice(exclaim8)
+
+response18=["Drop top, wasting time must stop stop","Drop top, Spongebob still waiting at the bus-stop"]
+random_response18=random.choice(response18)
+
+
+#Emotions
+exclaim0=["emotions",'Emotions']
+random_exclaim0=random.choice(exclaim0)
+
+exclaim3=['Good','good','Good!','good!','Not bad','Not bad!','not bad','not bad!']
+random_exclaim3=random.choice(exclaim3)
+exclaim03=['Bad','bad','Bad!','bad!']
+random_exclaim03=random.choice(exclaim03)
+
+exclaim4=['Yes!','Yes','yes!','yes','Yeah!','Yeah','yeah!','yeah','yep!','yep','Yep!','Yep']
+random_exclaim4=random.choice(exclaim4)
+exclaim04=['No!','no!','No','no','Nope!','nope!','nope','Nope']
+random_exclaim04=random.choice(exclaim04)
+exclaim004=['provide','Provide']
+random_exclaim004=random.choice(exclaim004)
+
+exclaim5=['Okay!','okay!','okay','Okay','OK','ok','OK!','ok!']
+random_exclaim5=random.choice(exclaim5)
+
+botq1=['How are you feeling right now? Good or bad?','How are you feeling? Good or bad?']
+random_botq1=random.choice(botq1)
+
+response7=["I'm happy that you're feeling good"]
+random_response7 = random.choice(response7)
+
+response8=["Everything will be all right. Are you stressed?"]
+random_response8 = random.choice(response8)
+
+response9=['Can I provide you some methods of relieving stress? (provide/no)']
+random_response9=random.choice(response9)
+
+response11=['Listen to calm music, especially ochestral music. Some good ochestral artists are Yo Yo Ma, Mozart, Beethoven.',
+"Talk to yourself and tell everything will be okay. Identify what is causing you to be stressed, is it the pile of work? Take a piece of paper and list down all the tasks you've got to do in an order. As you complete each task, cut it off from the list.", 
+"Breath! Close your eyes, take a deep breath to fill your lungs and exhale slowly. Repeat while concentrating on it.",
+"Make yourself laugh. Read or watch something funny. Laughing tricks your nervous system into making you happy.",
+"Take a short nap, or else talk(phone) with someone(friend) for a while."]
+random_response11=random.choice(response11)
+
+#Commands
+commands1=['Suggest a hobby','suggest a hobby','Hobby','hobby','Tell something to do','tell something to do']
+random_command1=random.choice(commands1)
+
+commands2=["create","Create"]
+random_command2=random.choice(commands2)
+
+commands3=["Say something cool","say something cool"]
+response12=["Blue whales heart is the size of a VW Beetle and that you could swim through some of its arteries",
+"All of the gold mined in the history of the world would more or less fit into a 20x20x20 meter cube",
+"Pirates wear eye patches so that they can see in the dark","Mayonnaise is made from oil and eggs","Goats have rectangular pupils"]
+random_response12=random.choice(response12)
+
+commands4=["Say something scary","say something scary"]
+response13=["A human head remains conscious for around 20 seconds after being decapitated.","About 153,000 people worldwide die everyday",
+"About 800,000 people commit suicide worldwide every year","During the filming of The Exorcist, multiple sets burnt down, actors got mysteriously injured and one of the actors Jack McGowran died of influenza. Eventually a priest was brought in to blessing the set.",
+"The Hoia Baciu is the most haunted forest in the world. Many mysterious things have been observed in the forest. To the center of the forest lies a circle where plant life doesn't grow, it is believed to be a portal to another dimension.",
+""]
+random_response13=random.choice(response13)
+
+commands5=["codes","Codes"]
+response15=["(Type 'emotions' to ask for stress advice)(Type: 'say something cool' for a cool fact)(Type: 'say something scary' for a scary fact)(Type: 'suggest a hobby'/'hobby' for a hobby suggestion)(Type: 'create' for a nice face)(Type: 'xoxo' for hugs)"]
+random_response15=random.choice(response15)
+
+response14=["<(^_^)>","<(*_*)>"]
+random_response14=random.choice(response14)
+
+response10=['Start reading. Read anything from books, magazines, newspapers, internet articles etc.',
+'Start collecting antiques','Listen to good music','Start gardening, grow a lot of greens','Try learning a new language',
+'Try cooking','Ride a bicycle','Write something; a poem, a song, a short story or anything else','Draw something',
+'Solve a puzzle','Repair something']
+random_response10=random.choice(response10)
+
+
+
+while True:
+	userInput = input('You: ').lower
+	    
+	#Greetings
+	if userInput in greetings:
+		print(random_greeting)
+		
+	elif userInput in greetings2:
+		print(random_greeting02)
+		
+	elif userInput in greetings3:
+		print(random_greeting03)
+		
+	elif userInput in greetings5:
+		print(random_greeting05)
+		
+	elif userInput in greetings6:
+	    print(random_greeting06)
+	
+	elif userInput in greetings7:
+	    print(random_greeting07)
+	
+	#Random_questions	
+	elif userInput in question1:
+		print(random_response1)
+		print(random_botq1)
+		
+	elif userInput in question2:
+		print(random_response2)
+		
+	elif userInput in question3:
+		print(random_response3)
+		
+	elif userInput in question4:
+		print(random_response4)
+		
+	elif userInput in question5:
+		print(random_response5)
+	
+	elif userInput in question6:
+		print(random_response6)
+		
+	elif userInput in question7:
+	    print(random_response16)
+		
+	
+	#
+	elif userInput in exclaim:
+		print(random_greeting4)
+		
+	elif userInput in exclaim6:
+	    print(random_greeting6)
+	    
+	elif userInput in exclaim5:
+	    print(random_exclaim5)
+	    
+	elif userInput in exclaim7:
+	    print(random_response17)
+		
+	elif userInput in exclaim8:
+	    print(random_response18)
+	    
+	
+	#Emotions	
+	elif userInput in exclaim0:
+		print(random_botq1)
+	elif userInput in exclaim3:
+		print(random_response7)
+	elif userInput in exclaim03:
+		print(random_response8)
+		
+	elif userInput in exclaim4:
+	    print(random_response9)
+	elif userInput in exclaim004:
+	    print(random_response11)
+	elif userInput in exclaim04:
+	    print(random_exclaim5)
+	    
+	#Commands
+	elif userInput in commands1:
+	    print(random_response10)
+	    
+	elif userInput in commands2:
+	    print(random_response14)
+	    
+	elif userInput in commands3:
+	    print(random_response12)
+	    
+	elif userInput in commands4:
+	    print(random_response13)
+	 
+	elif userInput in commands5:
+	    print(random_response15)
+	    
+	    
+
+	else:
+		print("I did not understand what you said. We bots are not intelligent like humans.")
+		
